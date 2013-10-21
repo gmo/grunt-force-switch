09f0d095234062838ee1d019d2fcd611d5b249a9
Jason Gill <jason.gill@gmomail.org>
2013-10-21 09:48:47 -0500

    Initial changes for force switch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,58 +16,36 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        'test/*_test.js'
       ],
       options: {
-        jshintrc: '.jshintrc',
-      },
+        jshintrc: '.jshintrc'
+      }
     },
 
-    // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ['tmp']
     },
 
-    // Configuration to be run (and then tested).
-    force_switch: {
-      default_options: {
+    mochaTest: {
+      all: {
         options: {
+          reporter: 'spec'
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
+        src: ['test/*_test.js']
+      }
     },
 
   });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'force_switch', 'nodeunit']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  // Tasks
+  grunt.registerTask('build', ['clean','jshint']);
+  grunt.registerTask('default', ['build', 'mochaTest']);
 
 };
--- tasks/force_switch.js
+++ tasks/force_switch.js
@@ -10,41 +10,21 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    grunt.registerTask('turnForceOn', 'Turn force on only if it has not already been set.', function() {
 
-  grunt.registerMultiTask('force_switch', 'A grunt plugin to turn force on and off', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+        if (!grunt.option('force')) {
+            grunt.config.set('usingForce', true);
+            grunt.option('force', true);
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
     });
-  });
+
+    grunt.registerTask('turnForceOff', 'Turn force off only if it was set using turnForceOn.', function() {
+
+        if (grunt.config.get('usingForce')) {
+            grunt.option('force', false);
+        }
+
+    });
 
 };
